2a0bc9530ae00c69621b9817146fb12b9e3accfe
wanjohiwamuchiri <wanjohimuchiri@protonmail.com>
2023-11-14 09:37:45 +0300

    changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # properman
+
+> repositories: This folder contains the interfaces and implementations for data access and persistence. The interfaces define the methods that can be used to interact with the data, and the implementations provide the specific implementation for that data source.
+
---
+++ pcmd/client/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	properman "github.com/wanjohiwamuchiri/properman/domain/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := properman.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &properman.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+	r, err = c.SayHelloAgain(ctx, &properman.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
